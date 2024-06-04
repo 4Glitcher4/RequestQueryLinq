e2241d2b955a3fe694e240c997f09a02319d0058
4Glitcher4 <n.black@gosu.kz>
2024-06-04 11:45:19 +0500

    Add project files.

---
+++ RQLinq/ExpressionHelper.cs
@@ -0,0 +1,203 @@
+﻿using System.Linq.Expressions;
+using System.Reflection;
+
+namespace RQLinq
+{
+    public static class ExpressionHelper
+    {
+        public static IQueryable<T> ApplySort<T>(this IQueryable<T> queryable, string sort)
+        {
+            if (!string.IsNullOrEmpty(sort))
+            {
+                var sorts = sort.Split(',')
+                    .Select(s => s.Trim().Split(' '))
+                    .Select(s => new
+                    {
+                        Field = s[0],
+                        Direction = s[1].ToLower() == "asc" ? nameof(Enumerable.OrderBy) : nameof(Enumerable.OrderByDescending)
+                    })
+                    .ToList();
+
+                var parameter = Expression.Parameter(typeof(T), "x");
+                var orderByExp = queryable.Expression;
+
+                foreach (var sortItem in sorts)
+                {
+                    var property = Expression.Property(parameter, sortItem.Field);
+                    orderByExp = Expression.Call(
+                        typeof(Queryable),
+                        sortItem.Direction,
+                        new[] { typeof(T), property.Type },
+                        orderByExp,
+                        Expression.Lambda(property, parameter));
+                }
+
+                queryable = queryable.Provider.CreateQuery<T>(orderByExp);
+                return queryable;
+            }
+            return queryable;
+        }
+
+        //public static int Count<T>(this IQueryable<T> queryable)
+        //{
+        //    var countExpression = Expression.Call(
+        //           typeof(Queryable),
+        //           nameof(Enumerable.Count),
+        //           new[] { typeof(T) },
+        //           queryable.Expression);
+
+
+        //    var countLambda = Expression.Lambda<Func<int>>(countExpression);
+        //    return queryable.Provider.Execute<int>(countExpression);
+        //}
+
+        public static IQueryable<T> ApplyFilter<T>(this IQueryable<T> queryable, List<FieldFilter> filters)
+        {
+            foreach (var filter in filters)
+            {
+                var field = filter.Field;
+                var value = filter.Value;
+
+                // Создаем выражение для фильтрации
+                ParameterExpression parameter = Expression.Parameter(typeof(T), "x");
+                Expression property = GetPropertyExpression(parameter, field);
+                ConstantExpression constant = Expression.Constant(value, value.GetType());
+                BinaryExpression comparison;
+                LambdaExpression lambda;
+                MethodCallExpression methodCallExpression;
+
+                switch (filter.Operator)
+                {
+                    case "gt":
+                        comparison = Expression.GreaterThan(property, constant);
+                        break;
+                    case "ls":
+                        comparison = Expression.LessThan(property, constant);
+                        break;
+                    case "eq":
+                        comparison = Expression.Equal(property, constant);
+                        break;
+                    case "in":
+                        ConstantExpression[] values = ((object[])filter.Value).Select(val => Expression.Constant(val, property.Type)).ToArray();
+                        NewArrayExpression constantArray = Expression.NewArrayInit(property.Type, values);
+                        MethodInfo inMethod = typeof(Enumerable).GetMethods()
+                            .First(m => m.Name == nameof(Enumerable.Contains) && m.GetParameters().Length == 2)
+                            .MakeGenericMethod(property.Type);
+
+                        MethodCallExpression containsMethodCall = Expression.Call(inMethod, constantArray, property);
+
+                        lambda = Expression.Lambda(containsMethodCall, parameter);
+
+                        methodCallExpression = Expression.Call(
+                            typeof(Queryable),
+                            nameof(Enumerable.Where),
+                            new[] { typeof(T) },
+                            queryable.Expression,
+                            lambda);
+
+                        queryable = queryable.Provider.CreateQuery<T>(methodCallExpression);
+                        continue;
+                    case "con":
+                        if (property.Type == typeof(string))
+                        {
+                            MethodInfo toLowerMethod = typeof(string).GetMethod(nameof(string.ToLower), Type.EmptyTypes);
+                            MethodCallExpression propertyToLower = Expression.Call(property, toLowerMethod);
+                            MethodCallExpression constantToLower = Expression.Call(constant, toLowerMethod);
+
+                            MethodInfo containsMethod = typeof(string).GetMethod(nameof(string.Contains), new[] { typeof(string) });
+                            MethodCallExpression containsMethodExp = Expression.Call(propertyToLower, containsMethod, constantToLower);
+
+                            lambda = Expression.Lambda(containsMethodExp, parameter);
+
+                            methodCallExpression = Expression.Call(
+                                typeof(Queryable),
+                                nameof(Enumerable.Where),
+                                new[] { typeof(T) },
+                                queryable.Expression,
+                                lambda);
+
+                            queryable = queryable.Provider.CreateQuery<T>(methodCallExpression);
+                            continue;
+                        }
+                        throw new ArgumentException($"The 'con' operator can only be used with string fields.");
+                    default:
+                        continue;
+                }
+
+                lambda = Expression.Lambda(comparison, parameter);
+                methodCallExpression = Expression.Call(
+                    typeof(Queryable),
+                    nameof(Enumerable.Where),
+                    new[] { typeof(T) },
+                    queryable.Expression,
+                    lambda);
+
+                queryable = queryable.Provider.CreateQuery<T>(methodCallExpression);
+            }
+
+            return queryable;
+        }
+
+        public static IQueryable<T> Skip<T>(this IQueryable<T> queryable, string skip)
+        {
+            if (!string.IsNullOrEmpty(skip))
+            {
+                return queryable.Provider.CreateQuery<T>(
+                Expression.Call(
+                        typeof(Queryable),
+                        nameof(Enumerable.Skip),
+                        new[] { typeof(T) },
+                        queryable.Expression,
+                        Expression.Constant(int.Parse(skip))));
+
+            }
+
+            return queryable;
+        }
+
+        public static IQueryable<T> Take<T>(this IQueryable<T> queryable, string take)
+        {
+            if (!string.IsNullOrEmpty(take))
+            {
+                return queryable.Provider.CreateQuery<T>(
+                Expression.Call(
+                        typeof(Queryable),
+                        nameof(Enumerable.Take),
+                        new[] { typeof(T) },
+                        queryable.Expression,
+                        Expression.Constant(int.Parse(take))));
+            }
+
+            return queryable;
+        }
+
+        private static Expression GetPropertyExpression(ParameterExpression parameter, string fieldName)
+        {
+            if (fieldName.Contains('.'))
+            {
+                // Если название поля содержит точку, это вложенное свойство
+                string[] propertyNames = fieldName.Split('.');
+                Expression propertyExpression = parameter;
+
+                foreach (var propertyName in propertyNames)
+                {
+                    propertyExpression = Expression.Property(propertyExpression, propertyName);
+                }
+
+                return propertyExpression;
+            }
+            else
+            {
+                // Иначе это свойство верхнего уровня
+                return Expression.Property(parameter, fieldName);
+            }
+        }
+    }
+
+    public class FieldFilter
+    {
+        public string Field { get; set; }
+        public string Operator { get; set; }
+        public object Value { get; set; }
+    }
+}
---
+++ RQLinq/QueryAttribute.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.AspNetCore.Mvc;
+using System.Reflection;
+using System.Text.RegularExpressions;
+
+namespace RQLinq
+{
+    public class QueryAttribute<T> : ActionFilterAttribute
+    {
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            var filter = context.HttpContext.Request.Query["$filter"].ToString();
+            var sort = context.HttpContext.Request.Query["$sort"].ToString();
+            var take = context.HttpContext.Request.Query["$take"].ToString();
+            var skip = context.HttpContext.Request.Query["$skip"].ToString();
+
+            var filters = ParseFilters(filter);
+            // Применяем фильтры к результату запроса
+            (IQueryable result, int count) = ((IQueryable, int))ApplyFilters(context.Result as OkObjectResult, filters, skip, take, sort);
+
+            // Заменяем результат запроса отфильтрованным результатом
+            var executedResult = context.Result as ObjectResult;
+            executedResult.Value = new { count, result };
+
+        }
+
+        private dynamic ParseFilters(string filter)
+        {
+            //var regex = new Regex(@"(\w+)\s*(eq|gt|ls|in|con)\s*'([\w.-]+)'");
+            //var regex = new Regex(@"(\w+)\s*(eq|gt|ls|contains|in)\s*(\(.+?\)|'[^']*'|[\w.-]+)");
+            var regex = new Regex(@"([\w.]+)\s*(eq|gt|ls|contains|in)\s*(\(.+?\)|'[^']*'|[\w.-]+)");
+            var matches = regex.Matches(filter);
+
+            var filters = matches.Select(match =>
+            {
+                var field = match.Groups[1].Value;
+                var operatorType = match.Groups[2].Value;
+                var valueString = match.Groups[3].Value.Trim('\'', '(', ')');
+
+                object value;
+                if (operatorType == "in")
+                {
+                    value = valueString.Split(new[] { "', '" }, StringSplitOptions.RemoveEmptyEntries)
+                                       .Select(v => ConvertToType(field, v))
+                                       .ToArray();
+                }
+                else
+                {
+                    value = ConvertToType(field, valueString);  // Преобразуем значение в соответствующий тип
+                }
+
+                return new FieldFilter
+                {
+                    Field = field,
+                    Operator = operatorType,
+                    Value = value
+
+                };
+            }).ToList();
+
+            return filters;
+        }
+
+        private object ConvertToType(string fieldName, string value)
+        {
+            var propertyInfo = GetPropertyInfo(typeof(T), fieldName);
+            if (propertyInfo == null) throw new ArgumentException($"Field '{fieldName}' not found on type '{typeof(T).Name}'");
+
+            var propertyType = propertyInfo.PropertyType;
+
+            if (propertyType.IsEnum)
+            {
+                return Enum.Parse(propertyType, value, true);
+            }
+
+            if (propertyType.Namespace == nameof(System))
+            {
+                return Convert.ChangeType(value, propertyType);
+            }
+
+            if (propertyType == typeof(Guid))
+            {
+                return Guid.Parse(value);
+            }
+
+            if (fieldName.Contains("/"))
+            {
+
+            }
+
+            return Convert.ChangeType(value, propertyType);
+        }
+
+        private PropertyInfo GetPropertyInfo(Type type, string propertyPath)
+        {
+            PropertyInfo propertyInfo = null;
+            foreach (var propertyName in propertyPath.Split('.'))
+            {
+                propertyInfo = type.GetProperty(propertyName, BindingFlags.IgnoreCase | BindingFlags.Public | BindingFlags.Instance);
+                if (propertyInfo == null) return null;
+                type = propertyInfo.PropertyType;
+            }
+            return propertyInfo;
+        }
+
+        private (IQueryable result, int count) ApplyFilters(OkObjectResult result, List<FieldFilter> filters, string skip, string take, string sort)
+        {
+            var queryable = result.Value as IQueryable<T>;
+
+            if (queryable != null)
+            {
+                var count = queryable.Count();
+                // Применяем сортировку
+                var results = queryable.ApplyFilter(filters)
+                    .ApplySort(sort)
+                    .Skip(skip)
+                    .Take(take);
+
+                return (results, count);
+            }
+            return (queryable, 0);
+        }
+    }
+}
