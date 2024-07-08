4ce76f61d00f0c23a3ed874e5c9ac088847af6ec
4Glitcher4 <n.black@gosu.kz>
2024-07-08 18:54:20 +0500

    Chore change hot fix

--- RequestQueryLinq/QueryAttribute.cs
+++ RequestQueryLinq/QueryAttribute.cs
@@ -35,7 +35,7 @@ namespace RequestQueryLinq
                 //var regex = new Regex(@"([\w./]+)\s*(eq|gt|ls|contains|in)\s*('[^']*'|\(.+?\)|[\w.-]+)");
                 //var regex = new Regex(@"([\w./]+)\s*(eq|nq|and|or|gt|ls|contains|in|any)\s*(\([^)]+\)|'[^']*'|[\w.-]+)");
                 //var regex = new Regex(@"([\w./]+)\s*(eq|nq|and|or|gt|ls|contains|in|any)\s*(\(.+?\)|'[^']*'|[\w.-]+)");
-                var regex = new Regex(@"([\w./]+)\s*(eq|nq|and|or|gt|gte|lt|lte|contains|ncontains|in|nin|.any)\s*(\([^)]+\)|'[^']*'|[\w.-]+)");
+                var regex = new Regex(@"([\w./]+)\s*(eq|nq|and|or|gt|gte|ls|lse|contains|ncontains|in|nin|.any)\s*(\([^)]+\)|'[^']*'|[\w.-]+)");
                 var matches = regex.Matches(filter);
 
                 var filters = matches.Select(match =>
